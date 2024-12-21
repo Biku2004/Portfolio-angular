efbd29aebc66f6ab1ac21e64fdbce94ef1801126
Biku2004 <bikash.behera24.04.2004@gmail.com>
2024-12-21 22:12:37 +0530

    added profit and loss in the stock table

--- Simple-portfolio-tracker/src/app/dashboard/stock-list/stock-list.component.ts
+++ Simple-portfolio-tracker/src/app/dashboard/stock-list/stock-list.component.ts
@@ -1,107 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { Router } from '@angular/router';
-// import { StockService } from '../../services/StockService';
-
-// interface Stock {
-//   id?: number;
-//   name: string;
-//   ticker: string;
-//   quantity: number;
-//   buyPrice: number;
-//   // currentPrice : number;
-//   // lossProfit: number;
-// }
-
-
-// @Component({
-//   selector: 'app-stock-list',
-//   standalone: true,
-//   imports: [CommonModule],
-//   templateUrl: './stock-list.component.html',
-//   styleUrls: ['./stock-list.component.css'] // Fixed property name
-// })
-
-// export class StockListComponent implements OnInit {
-//   stocks: Stock[] = [];
-
-//   constructor(
-//     private stockService: StockService, 
-//     private router: Router) { }
-
-//   ngOnInit(): void {
-//     this.loadStocks();
-//   }
-
-//   loadStocks() {
-//     this.stockService.getStocks().subscribe((stocks: Stock[]) => {
-//       this.stocks = stocks;
-//     });
-//   }
-
-//   editStock(stock: Stock) {
-//     this.router.navigate(['/edit-stock', stock.id]);
-//   }
-
-//   deleteStock(stock: Stock) {
-//     if (stock.id !== undefined) {
-//       this.stockService.deleteStock(stock.id).subscribe(response => {
-//         this.loadStocks();
-//       });
-//     }
-//   }
-// }
-
-// import { Component, OnInit } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { Router } from '@angular/router';
-// import { CrudService } from '../../services/crud.service';
-
-// interface Stock {
-//   id?: number;
-//   name: string;
-//   ticker: string;
-//   quantity: number;
-//   buyPrice: number;
-// }
-
-// @Component({
-//   selector: 'app-stock-list',
-//   standalone: true,
-//   imports: [CommonModule],
-//   templateUrl: './stock-list.component.html',
-//   styleUrls: ['./stock-list.component.css']
-// })
-// export class StockListComponent implements OnInit {
-//   stocks: Stock[] = [];
-
-//   constructor(
-//     private crudService: CrudService, 
-//     private router: Router) {}
-
-//   ngOnInit(): void {
-//     this.loadStocks();
-//   }
-
-//   loadStocks(): void {
-//     this.crudService.getStocks().subscribe((stocks: Stock[]) => {
-//       this.stocks = stocks;
-//     });
-//   }
-
-//   editStock(stock: Stock): void {
-//     this.router.navigate(['/edit-stock', stock.id]);
-//   }
-
-//   deleteStock(stock: Stock): void {
-//     if (stock.id) {
-//       this.crudService.deleteStock(stock.id).subscribe(() => {
-//         this.loadStocks();
-//       });
-//     }
-//   }
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
@@ -115,6 +11,11 @@ interface Stock {
   ticker: string;
   quantity: number;
   buyPrice: number;
+  currentPrice?: number;
+  totalValue?: number;
+  profitLoss?: number;
+  profitLossPercent?: number;
+
 }
 
 @Component({
@@ -146,6 +47,29 @@ export class StockListComponent implements OnInit {
   loadStocks() {
     this.crudService.getStocks().subscribe((stocks: Stock[]) => {
       this.stocks = stocks;
+      this.stocks.forEach(stock => {
+        this.updateCurrentPrice(stock);
+      });
+    });
+  }
+
+  // updateCurrentPrice(stock: Stock) {
+  //   this.stockService.getStockPrice(stock.ticker).subscribe(data => {
+  //     stock.currentPrice = data.c;
+  //     if (stock.currentPrice !== undefined) {
+  //       stock.totalValue = stock.currentPrice * stock.quantity;
+  //     }
+  //   });
+  // }
+
+  updateCurrentPrice(stock: Stock) {
+    this.stockService.getStockPrice(stock.ticker).subscribe(data => {
+      stock.currentPrice = data.c;
+      if (stock.currentPrice !== undefined) {
+        stock.totalValue = stock.currentPrice * stock.quantity;
+        stock.profitLoss = (stock.currentPrice - stock.buyPrice) * stock.quantity;
+        stock.profitLossPercent = ((stock.currentPrice - stock.buyPrice) / stock.buyPrice) * 100;
+      }
     });
   }
 
