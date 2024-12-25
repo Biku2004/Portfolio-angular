e7bb5160613a9634dca29d84b56e9f5813e4b7ac
Biku2004 <bikash.behera24.04.2004@gmail.com>
2024-12-25 12:16:15 +0530

    aws link changes for api

--- Simple-portfolio-tracker/src/app/services/marketStatus.service.ts
+++ Simple-portfolio-tracker/src/app/services/marketStatus.service.ts
@@ -15,7 +15,7 @@ interface MarketStatus {
   providedIn: 'root'
 })
 export class MarketStatusService {
-  private apiUrl = 'http://localhost:8080/api/stocks';
+  private apiUrl = 'http://backend-portfolio-env.eba-hb9e3thu.ap-south-1.elasticbeanstalk.com:8080/api/stocks';
 
   constructor(private http: HttpClient) {}
 
