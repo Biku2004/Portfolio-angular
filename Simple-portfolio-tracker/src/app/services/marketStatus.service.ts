6272052a32631b8b62095af88ed6200722e8b3e7
Biku2004 <bikash.behera24.04.2004@gmail.com>
2025-04-10 20:03:45 +0530

    Revert "aws Elastic beanstalk link chnage"
    This reverts commit 9eab528a934b4c86919b70c63ec43dcc65dfac15.

--- Simple-portfolio-tracker/src/app/services/marketStatus.service.ts
+++ Simple-portfolio-tracker/src/app/services/marketStatus.service.ts
@@ -15,7 +15,7 @@ interface MarketStatus {
   providedIn: 'root'
 })
 export class MarketStatusService {
-  private apiUrl = 'https://backend-env.eba-mpeky2ew.ap-south-1.elasticbeanstalk.com/api/stocks';
+  private apiUrl = 'http://backend-env.eba-mpeky2ew.ap-south-1.elasticbeanstalk.com/api/stocks';
 
   constructor(private http: HttpClient) {}
 
--- Simple-portfolio-tracker/src/app/services/stock.service.ts
+++ Simple-portfolio-tracker/src/app/services/stock.service.ts
@@ -27,7 +27,7 @@ interface BasicFinancials {
   providedIn: 'root'
 })
 export class StockService {
-  private apiUrl = 'https://backend-env.eba-mpeky2ew.ap-south-1.elasticbeanstalk.com/api/stocks';
+  private apiUrl = 'http://backend-env.eba-mpeky2ew.ap-south-1.elasticbeanstalk.com/api/stocks';
 
   constructor(private http: HttpClient) {}
 
